5e1f89e1c727880d8b3de0ce017fc3defc567736
BernardoAlvesCostaBomtempoMachado <105887787+BernardoAlvesCostaBomtempoMachado@users.noreply.github.com>
2024-09-17 10:58:35 -0300

    lll

---
+++ perguntas.js
@@ -0,0 +1,96 @@
+criaCartao(
+    'Matemática',
+    'Qual a fórmula da área de um triângulo?',
+   'Área = (base * altura) / 2',
+)
+    criaCartao(
+   'Matemática',
+    'Qual a fórmula da equação do segundo grau?',
+    'ax² + bx + c = 0',
+    )
+    criaCartao(
+    'História',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington',
+    )
+    criaCartao(
+    'História',
+    'Qual foi a principal causa da Revolução Francesa?',
+    'Desigualdade social e crise econômica',
+    )
+    criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'H₂O',
+    )
+    criaCartao(
+    'Química',
+    'O que é uma reação de neutralização?',
+    'É a reação entre um ácido e uma base que produz água e sal',
+    )  
+    criaCartao(
+    'Física',
+    'Qual a fórmula da força?',
+    'F = m * a',
+    )
+    criaCartao(
+    'Física',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'Newton (N)',
+    )
+    criaCartao(
+    'Biologia',
+    'O que é a fotossíntese?',
+    'Processo pelo qual as plantas convertem luz solar em energia química',
+    )
+    criaCartao(
+    'Biologia',
+    'Qual é o principal órgão do sistema circulatório?',
+    'O coração',
+    )
+    criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'Ásia',
+    )
+    criaCartao(
+    'Geografia',
+    'Qual é o nome do maior oceano?',
+    'Oceano Pacífico',
+    )
+    criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis',
+    )
+    criaCartao(
+    'Literatura',
+    'Qual é o período histórico em que se passa a obra "O Guarani"?',
+    'Brasil Colônia',
+    )
+    criaCartao(
+    'Filosofia',
+    'Quem é o autor da obra “A República”?',
+    'Platão',
+    )
+    criaCartao(
+    'Filosofia',
+    'Qual é a principal ideia do existencialismo?',
+    'A liberdade individual e a responsabilidade pessoal',
+    )
+    criaCartao(
+    'Inglês',
+    'Como se diz “livro” em inglês?',
+    'Book',
+    )
+    criaCartao(
+    'Inglês',
+    'Qual é o passado simples do verbo “to go”?',
+    'Went',
+    )
+    criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?"',
+    'Leonardo da Vinci',
+    )
+     
